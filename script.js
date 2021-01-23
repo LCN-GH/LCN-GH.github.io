65e2bc135aadc92585f1d91dd0c353e7feaf2734
LCN-GH <lewys.norton@gmail.com>
2021-01-23 21:39:39 +0000

    initial commit
    first draft

---
+++ script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+$(document).ready(function () {
+    $('.nav-button').click(function () {
+        $('.nav-button').toggleClass('change');
+    });
+
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        if (position >= 200) {
+            $('.nav-menu').addClass('custom-navbar');
+        } else {
+            $('.nav-menu').removeClass('custom-navbar');
+        }
+    });
+
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        if (position >= 650) {
+            $('.camera-img').addClass('fromLeft');
+            $('.mission-text').addClass('fromRight');
+        } else {
+            $('.camera-img').removeClass('fromLeft');
+            $('.mission-text').removelass('fromRight');
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
