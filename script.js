206954a7d927506b2fc93f79d9e9183d60e9180b
LCN-GH <lewys.norton@gmail.com>
2021-05-26 16:24:55 +0100

    more fixes
    applied DRY to script.js and fixed pricing animation
    removed useless code from stylesheet

--- script.js
+++ script.js
@@ -11,24 +11,13 @@ $(document).ready(function () {
             $('.nav-menu').addClass('custom-navbar');
         } else {
             $('.nav-menu').removeClass('custom-navbar');
-        }
-    });
-
-    $(window).scroll(function () {
-        let position = $(this).scrollTop();
-        if (position >= 650) {
+        } if (position >= 650) {
             $('.camera-img').addClass('fromLeft');
             $('.mission-text').addClass('fromRight');
         } else {
             $('.camera-img').removeClass('fromLeft');
             $('.mission-text').removeClass('fromRight');
-        }
-    });
-
-
-    $(window).scroll(function () {
-        let position = $(this).scrollTop();
-        if (position >= 4250) {
+        } if (position >= 2300) {
             $('.card-1').addClass('moveFromLeft');
             $('.card-2').addClass('moveFromBottom');
             $('.card-3').addClass('moveFromRight');
@@ -39,19 +28,7 @@ $(document).ready(function () {
         }
     });
 
-    $('.gallery-list-item').click(function () {
-        let value = $(this).attr('data-filter');
-        if (value === 'all') {
-            $('.filter').show(300);
-        } else {
-            $('.filter').not('.' + value).hide(300);
-            $('.filter').filter('.' + value).show(300);
-        }
-    });
 
-    $('.gallery-list-item').click(function () {
-        $(this).addClass('active-item').siblings().removeClass('active-item');
-    });
 });
 
 
