2d0adcd36258ed8b0a0e33d30c771e1288ae2f4c
LCN-GH <lewys.norton@gmail.com>
2021-05-26 14:07:32 +0100

    portfolio
    last files were tests this is my portfolio from march before ongoing updates

--- script.js
+++ script.js
@@ -24,6 +24,34 @@ $(document).ready(function () {
             $('.mission-text').removeClass('fromRight');
         }
     });
+
+
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        if (position >= 4250) {
+            $('.card-1').addClass('moveFromLeft');
+            $('.card-2').addClass('moveFromBottom');
+            $('.card-3').addClass('moveFromRight');
+        } else {
+            $('.card-1').removeClass('moveFromLeft');
+            $('.card-2').removeClass('moveFromBottom');
+            $('.card-3').removeClass('moveFromRight');
+        }
+    });
+
+    $('.gallery-list-item').click(function () {
+        let value = $(this).attr('data-filter');
+        if (value === 'all') {
+            $('.filter').show(300);
+        } else {
+            $('.filter').not('.' + value).hide(300);
+            $('.filter').filter('.' + value).show(300);
+        }
+    });
+
+    $('.gallery-list-item').click(function () {
+        $(this).addClass('active-item').siblings().removeClass('active-item');
+    });
 });
 
 
